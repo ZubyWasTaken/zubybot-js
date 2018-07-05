1e72519783c557a066b487b922ff70c0c49488c5
ZubyWasTaken <zubairkhalid12@gmail.com>
2018-07-05 18:27:53 +0100

    Detects if color name exists. Need to find a way to also return the hex code of that color at the position it finds it at - then create role.

--- bot.js
+++ bot.js
@@ -37,113 +37,163 @@ const embed = new Discord.RichEmbed()
 
 
 client.on("message", (message) => {
-        // Exit and stop if it's not there
-        if (message.author.bot) return; // if the author of the message is the bot do nothing
+    // Exit and stop if it's not there
+    if (message.author.bot) return; // if the author of the message is the bot do nothing
 
-        if (!message.content.startsWith(config.prefix)) return;
+    if (!message.content.startsWith(config.prefix)) return;
 
-        if (message.content.startsWith(config.prefix + "help")) { // if message is !help
-            message.channel.send(embed); // send an embed
-        }
-        if (message.author.id == config.zubyID) { // if message author has this specific ID
-            if (message.content.startsWith(config.prefix + "addcolor")) { // and if the message is !addcolor
-                inputCommand = "".concat(message.content.split('!addcolor ', 0))
+    if (message.content.startsWith(config.prefix + "help")) { // if message is !help
+        message.channel.send(embed); // send an embed
+    }
+    if (message.author.id == config.zubyID) { // if message author has this specific ID
+        if (message.content.startsWith(config.prefix + "addcolor")) { // and if the message is !addcolor
+            inputCommand = "".concat(message.content.split('!addcolor ', 0))
 
-                var inputCommand = message.content.split(" ")
+            var inputCommand = message.content.split(" ")
 
-                if (inputCommand.length > 3) { // checks if there are more than 3 parameters including addcolor
-                    message.channel.send("Too many arguments, please do !addcolor <hex> <colorname>");
-                } else if (inputCommand.length < 3) { // checks if there are less than 3 parameters including addcolor
-                    message.channel.send("Not enough arguments, please do !addcolor <hex> <colorname>");
-                } else {
-                    var colorHex = inputCommand[1]; // sets hex of color
-                    var colorName = inputCommand[2]; // sets name of color
-                    var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
+            if (inputCommand.length > 3) { // checks if there are more than 3 parameters including addcolor
+                message.channel.send("Too many arguments, please do !addcolor <hex> <colorname>");
+            } else if (inputCommand.length < 3) { // checks if there are less than 3 parameters including addcolor
+                message.channel.send("Not enough arguments, please do !addcolor <hex> <colorname>");
+            } else {
+                var colorHex = inputCommand[1]; // sets hex of color
+                var colorName = inputCommand[2]; // sets name of color
+                var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
 
-                    /*
-                    This function adds the color
-                    when given the color name and hex
-                     */
-                    function AddColor(colorName, colorHex) {
-                        var oldJson = JSON.parse(fs.readFileSync('colors.json', 'utf8'));
-                        oldJson['colors'].push({[colorName]: colorHex});
-                        var newJson = JSON.stringify(oldJson);
-                        fs.writeFileSync('colors.json', newJson);
+                /*
+                This function adds the color
+                when given the color name and hex
+                 */
+                function AddColor(colorName, colorHex) {
+                    var oldJson = JSON.parse(fs.readFileSync('colors.json', 'utf8'));
+                    oldJson['colors'].push({[colorName]: colorHex});
+                    var newJson = JSON.stringify(oldJson);
+                    fs.writeFileSync('colors.json', newJson);
 
-                        // Tells use the name of color they added and hex of that color
-                        message.channel.send("Added color " + colorName + " with hex value " + colorHex);
-                    }
-
-                    /*
-                    Checks if color exists.
-                    Returns either -1 or the index of the existing color
-                     */
-                    var colorExist = GetColorIndex(colorName)
-                    if (colorExist > -1) {
-                        // Prints only if the color exists
-                        message.channel.send("This color already exists")
-                    } else if (colorExist == -1) {
-                        /*
-                        If color does not exist then execute AddColor function.
-                        Passes in the name of color and hex.
-                         */
-                        message.channel.send("This color DOES NOT exist.")
-                        AddColor(colorName, colorHex)
-                    }
+                    // Tells use the name of color they added and hex of that color
+                    message.channel.send("Added color " + colorName + " with hex value " + colorHex);
                 }
-            }
-            if (message.content.startsWith(config.prefix + "deletecolor")) { // and if the message is !deletrcolor
-                inputCommand = "".concat(message.content.split('!deletecolor', 0))
 
-                var inputCommand = message.content.split(" ")
-
-                if (inputCommand.length > 2) {
-                    message.channel.send("Too many arguments, please do !deletecolor <colorname>");
-                } else if (inputCommand.length < 2) {
-                    message.channel.send("Not enough arguments, please do !deletecolor <colorname>");
-                } else {
-                    var colorName = inputCommand[1];  // sets name of color that is to be deleted
-                    var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
-
+                /*
+                Checks if color exists.
+                Returns either -1 or the index of the existing color
+                 */
+                var colorExist = GetColorIndex(colorName)
+                if (colorExist > -1) {
+                    // Prints only if the color exists
+                    message.channel.send("This color already exists")
+                } else if (colorExist == -1) {
                     /*
-                    Checks if color exists.
-                    Returns either -1 or the index of the existing color
+                    If color does not exist then execute AddColor function.
+                    Passes in the name of color and hex.
                      */
-                    var colorExist = GetColorIndex(colorName)
-                    if (colorExist == -1) {
-                        // If the color does not exist, it does nothing i.e not delete anything
-                        message.channel.send("This color DOES NOT exists")
-                    } else if (colorExist > -1) {
-                        /*
-                        If the color exists delete the position in the JSON.
-                        Write the edited JSON to the same file, overwriting it.
-                         */
-                        message.channel.send("This color ALREADY exists at position " + colorExist)
-                        message.channel.send("I am deleting color " + colorName)
-                        json["colors"].splice(colorExist, 1); //delete the position where the color is
-                        var newJson = JSON.stringify(json); // set to new json string
-                        fs.writeFileSync('colors.json', newJson); //writes to new json file
-                        message.channel.send("i have deleted " + colorName + " at position " + colorExist) //outputs what color got deleted and position
-                    }
+                    message.channel.send("This color DOES NOT exist.")
+                    AddColor(colorName, colorHex)
                 }
             }
         }
-        if (message.content.startsWith(config.prefix + "color")) { // and if the message is !deleterole
-            inputCommand = "".concat(message.content.split('!color', 0))
+        if (message.content.startsWith(config.prefix + "deletecolor")) { // and if the message is !deletrcolor
+            inputCommand = "".concat(message.content.split('!deletecolor', 0))
 
             var inputCommand = message.content.split(" ")
 
             if (inputCommand.length > 2) {
-                message.channel.send("Too many arguments, please do !color <colorname>");
-            } else if (inputCommand.length < 0) {
-                message.channel.send("Not enough arguments, please do !color <colorname>");
+                message.channel.send("Too many arguments, please do !deletecolor <colorname>");
+            } else if (inputCommand.length < 2) {
+                message.channel.send("Not enough arguments, please do !deletecolor <colorname>");
             } else {
-                var roleToGive = inputCommand[1];  // sets name of color that is to be deleted
+                var colorName = inputCommand[1];  // sets name of color that is to be deleted
+                var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
 
+                /*
+                Checks if color exists.
+                Returns either -1 or the index of the existing color
+                 */
+                var colorExist = GetColorIndex(colorName)
+                if (colorExist == -1) {
+                    // If the color does not exist, it does nothing i.e not delete anything
+                    message.channel.send("This color DOES NOT exists")
+                } else if (colorExist > -1) {
+                    /*
+                    If the color exists delete the position in the JSON.
+                    Write the edited JSON to the same file, overwriting it.
+                     */
+                    message.channel.send("This color ALREADY exists at position " + colorExist)
+                    message.channel.send("I am deleting color " + colorName)
+                    json["colors"].splice(colorExist, 1); //delete the position where the color is
+                    var newJson = JSON.stringify(json); // set to new json string
+                    fs.writeFileSync('colors.json', newJson); //writes to new json file
+                    message.channel.send("i have deleted " + colorName + " at position " + colorExist) //outputs what color got deleted and position
+                }
             }
         }
     }
-); //end of client.on("message", (message)
+    if (message.content.startsWith(config.prefix + "color")) { // and if the message is !deleterole
+        inputCommand = "".concat(message.content.split('!color', 0))
+
+        var inputCommand = message.content.split(" ")
+
+        if (inputCommand.length > 2) {
+            message.channel.send("Too many arguments, please do !color <colorname>");
+        } else if (inputCommand.length < 0) {
+            message.channel.send("Not enough arguments, please do !color <colorname>");
+        } else {
+            var roleToGive = inputCommand[1];  // sets name of color that is to be deleted
+            message.channel.send(roleToGive);
+
+            var color = '000001';
+
+            function DoesNameExist(color) {
+                'use strict';
+                const fs = require('fs');
+                var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
+                for (var i = 0; i < json["colors"].length; i++) { //loops through the length of the json file
+                    if (Object.keys(json["colors"][i])[0] == color) { //checks if the name is in the array
+                        return i; //returns position
+                    }
+                }
+                // only happens if the color does not exist
+                return -1;
+            }
+
+            function DoesColorExist(color) {
+                'use strict';
+                const fs = require('fs');
+                var json = JSON.parse(fs.readFileSync('colors.json', 'utf8')); // Opens json file to be read
+                for (var i = 0; i < json["colors"].length; i++) { //loops through the length of the json file
+                    if (Object.keys(json["colors"][i])[0] == color) { //checks if the name is in the array
+                        return Object.keys(json["colors"][i])[1];
+                    }
+                }
+                // only happens if the color does not exist
+                return -1;
+            }
+
+            var doesColorNameExist = DoesNameExist(roleToGive);
+
+            if (doesColorNameExist == -1) {
+                // If the color does not exist, it does nothing i.e not delete anything
+                message.channel.send("This color name does not exist")
+
+            } else if (doesColorNameExist > -1) {
+                message.channel.send("This color name does exist")
+                var hexColor = DoesColorExist(roleToGive)
+                message.channel.send(hexColor)
+            }
+
+
+
+            // message.member.guild.createRole({
+            //     name: `${roleToGive}`,
+            //     color: `0X${color}`,
+            //     permissons: []
+            // }).then(function(role){
+            //    message.member.addRole(role);
+            // });
+
+        }
+    }
+}); //end of client.on("message", (message)
 
 
 client.login(config.token);
